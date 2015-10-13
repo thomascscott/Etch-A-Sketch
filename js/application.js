b36e07950091b86372d236062cdbf7f56624cac5
Thomas Scott <thomas-scott@outlook.com>
2015-10-13 20:05:46 +1300

    Colours and opacity

--- js/application.js
+++ js/application.js
@@ -1,7 +1,9 @@
-var num = 4;
-var numsq;
-var sqD;
-var gridS;
+var num = 64;
+var numsq; //total number of squares
+var sqD; //size in pixels of squares
+var gridS; //size of grid, not quite fixed so as to overcome rounding errors
+var randColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+var opac; //here so available to reset fn
 
 $(document).ready(function(){
     defaultG();
@@ -11,6 +13,8 @@ $(document).ready(function(){
    $("#newGrid").on("click", function(){
         newGrid();
     });
+   $("input[name=mode]").click(highlightMode); //using the simple .click instead of .on
+   $("input[name=deepen]").click(newGrid);
 });
 
 function initialise(){
@@ -27,26 +31,44 @@ function defaultG(){
 }
 
 function highlightMode(){
-    $(".square").on("mouseenter", function(){
-        $(this).addClass("highlighted");
-    });
+    if($("input[name=mode][value='coloured']").is(":checked")){
+        $(".square").on("mouseenter", function(){
+            $(this).css({'background-color': randColor, 'opacity' : '+=0.1'});
+            randColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+        });
+    } else{
+        $(".square").on("mouseenter", function(){
+            $(this).css({'background-color': 'black', 'opacity' : '+=0.1'});
+        });
+    }
 }
 
 function reset(){
-    $("#grid").find(".square").removeClass("highlighted");
+    $("#grid").find(".square").css({"background-color": "white", "opacity": opac});
+    highlightMode();
 };
     
 function newGrid(){
+    num = window.prompt("Testing", num);
+    if(!num){ //if NULL, ie if cancel pressed
+        return; //highlightMode();
+    }
     $(".square").remove();
-    num = window.prompt("Testing", "64");
     initialise();
     createGrid();
 }
 
 function createGrid(){
+    opac = 1;
     for(var i = 0; i < numsq; i++){
         $("#grid").append("<div class='square'></div>");
     }
-    $(".square").css({"width": sqD, "height": sqD});
+    if($("input[name=deepen][value='deep']").is(":checked")){
+        opac = 0;
+    }
+    $(".square").css({"width": sqD, "height": sqD, "opacity": opac});
+    if(num > 100){ //1px is too large past a certain point
+        $(".square").css({"border": "none"});
+    }
     highlightMode();
 }
