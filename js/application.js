df0d1bb63fd20f6a333564efacb8b0933819f03d
Thomas Scott <thomas-scott@outlook.com>
2015-10-13 17:29:19 +1300

    Set grid size button

--- js/application.js
+++ js/application.js
@@ -1,22 +1,32 @@
 var num = 4;
-var numsq = num*num;
-var sqD = 200 / num; //dimensions of the squares
-var gridS = sqD * num + 0.01; //so as rounding errors don't result in non sqaure grids
+var numsq;
+var sqD;
+var gridS;
 
 $(document).ready(function(){
     defaultG();
     $("#reset").on("click", function(){
         reset();
     });
-   
+   $("#newGrid").on("click", function(){
+        newGrid();
+    });
 });
 
+function initialise(){
+    numsq = num*num;
+    sqD = 960 / num; //dimensions of the squares
+    gridS = sqD * num + 0.01; //so as rounding errors don't result in non sqaure grids
+}
+
 function defaultG(){
+    initialise();
     $("#grid").css({"width": gridS, "height": gridS});
-    for(var i = 0; i < numsq; i++){
-        $("#grid").append("<div class='square'></div>");
-    }
-    $(".square").css({"width": sqD, "height": sqD});
+    createGrid();
+    highlightMode();
+}
+
+function highlightMode(){
     $(".square").on("mouseenter", function(){
         $(this).addClass("highlighted");
     });
@@ -26,3 +36,17 @@ function reset(){
     $("#grid").find(".square").removeClass("highlighted");
 };
     
+function newGrid(){
+    $(".square").remove();
+    num = window.prompt("Testing", "64");
+    initialise();
+    createGrid();
+}
+
+function createGrid(){
+    for(var i = 0; i < numsq; i++){
+        $("#grid").append("<div class='square'></div>");
+    }
+    $(".square").css({"width": sqD, "height": sqD});
+    highlightMode();
+}
